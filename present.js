73a57791a633ffd7fd80f44c2431a5ddf982dc4a
FourN7 <nguyenngonhutnam@gmail.com>
2024-03-08 16:47:51 +0700

    init project

---
+++ present.js
@@ -0,0 +1,45 @@
+var to = 'Bông !';
+var gift_url = 'https://www.facebook.com/boon9minz';
+var gift_image_url = 'images/2024_03_08_14_56_IMG_0877.jpg';
+
+
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+
+function init() {
+  
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+    document.getElementById('card').classList.add('card-show')
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
+
